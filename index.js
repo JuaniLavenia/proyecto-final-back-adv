c5796aeb663992d2a2d8bb740074f1b869c21a3a
Juan Ignacio Lavenia <jlavenia@diloog.com.ar>
2024-04-02 21:38:15 -0300

    update base code

---
+++ index.js
@@ -0,0 +1,6 @@
+const { app } = require("./src/app.js");
+const PORT = process.env.PORT;
+
+app.listen(PORT, () =>
+  console.log(`Server running at: http://localhost:${PORT}`)
+);
---
+++ src/app.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("mongoose conectado"))
+  .catch((err) => console.log(err));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+app.use(express.json());
+
+const cors = require("cors");
+
+app.get("/", (req, res) => {
+  res.send("Bienvenido");
+});
+
+app.use(cors());
+
+module.exports = {
+  app,
+};
